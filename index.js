8bcec845c32ac40bb33de66e1186d5118dbd0a70
jonathanota <jonathanota1@gmail.com>
2020-05-18 11:42:40 -0700

    snapshot before adding Arduino LED control

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
 const matrix = require("@matrix-io/matrix-lite");
 var SunCalc = require('suncalc');
-const interpolate = require('color-interpolate');
+const interpolate = require('d3-interpolate');
 const interpolateColor = require('d3-color');
 const d3 = require('d3-interpolate');
 let mapsService = "https://maps.googleapis.com/maps/api/geocode/json"
@@ -15,20 +15,64 @@ let mapsKey = getKey("GeocodingKey");
 let globalLat = 0;
 let globalLong = 0;
 let locationSet = false;
+var motorPos = 0;
+var sunPos = 0;
+var prevMotorPos = 0;
+let sunAlt = 0;
+let sunFraction = 0;
+let maxHeight = 0;
+
+let solStatus = "";
 
 var mic = matrix.alsa.mic();
 
 // ----- Setup Serial Port ----- //
   //open the Port
   //make sure the port has been successfully opened
-
-  // const sol = new SerialPort("", function(err){
+  const port = "/dev/ttyACM0";
+  const sol = new SerialPort(port, {baudRate: 115200});
+  // const sol = new SerialPort(port, function(err){
   //     if (err){
   //       return console.log('Error: ', err.message)
   //     }
+  //     // autoOpen: false
+  //     // lock: true
+  //     baudRate: 115200
   // });
 
-  // const parser = sol.pipe(new Readline({ delimiter: '\n' }));
+  const parser = sol.pipe(new Readline({ delimiter: '\r\n', encoding: 'utf8' }));
+
+  sol.on("open", () => {
+    console.log('serial port open');
+
+    var terminal_input = process.stdin;
+    terminal_input.setEncoding('utf-8');
+    console.log("Ready to calibrate?");
+    //auto calibrate
+
+    // terminal_input.on('data', function(data){
+    //     sol.write(data);
+    // });
+  });
+
+  sol.on("close", () => {
+      open();
+  });
+
+
+  function open() {
+    sol.open(function (err) {
+        if (err){
+          console.log('Port is not open: ' + err.message);
+          setTimeout(open, 1000); // next attempt to open after 1s
+        }
+        else {
+        sol.resume();
+        return;
+        }
+    });
+}
+  // parser.on('data', console.log)
 
 // ----- Setup LED array ----- //
 let everloop = new Array(matrix.led.length);
@@ -42,36 +86,44 @@ if (everloop.length == 35) {
 
 // --- Check if file for location exists, if not ask to create one -- //
 locationFileCheck();
-// ------ Check to see if the location file is empty ------ //
-function locationFileCheck (){
-  fs.readFile('currentLocation.json', 'utf8', function(err, contents){
-    if (err){
-      console.log("Something went wrong reading the location file.");
-      return err;
-    }
-    var emptyFile = !Object.keys(contents).length;
-    console.log("Is the file empty?" + " " + emptyFile);
-    if (emptyFile){
-      askForLocation();
-    }
-    else {
-    getLocationInfo();
-    }
-  });
-}
+
 
 // --------- Run Sun ---------- //
 //setInterval sets how many time the linked to function runs
+if (!locationSet){
 setInterval(sunLight, 100);
+}
+
+// Read the port data
+parser.on('data', data =>{
+
+  // var newData = new TextEncoder("utf-8").encode(data);
+  // var existingData = new TextEncoder("utf-8").encode("ready");
+  // console.log(newData);
+  // console.log(existingData);
+
+  solStatus = data;
+
+  //Set the maximum height to sol
+  var solStatusSplit = solStatus.split(",");
+  if (solStatusSplit[0] == "maxHeight"){
+    maxHeight = parseInt(solStatusSplit[1]);
+    console.log(maxHeight);
+  }
+
+
+});
+
 
 // ------ Set sun behavior ----- //
 let dayColors = d3.interpolateRgbBasis(["rgb(225,76,6)","rgb(97,99,104)","rgb(255,255,255)"]);
 let twilightColors = d3.interpolateRgbBasis(["rgb(225,76,6)","rgb(26,14,21)","rgb(0,0,0)"]);
 function sunLight(){
-  let maxSunAlt = 68;
+  let maxSunAlt = 80;
   let minSunAlt = -6.3;
-  let sunFraction = whatsUpSun(globalLat, globalLong)/maxSunAlt;
-  let twilightFraction = whatsUpSun(globalLat, globalLong)/minSunAlt;
+  sunAlt = whatsUpSun(globalLat, globalLong);
+  sunFraction = sunAlt/maxSunAlt;
+  let twilightFraction = sunAlt/minSunAlt;
   // console.log("Twilight Fraction:" + " " + twilightFraction);
   // console.log(twilightColors(twilightFraction).toString());
   if (sunFraction > 0 && sunFraction < 1){
@@ -84,13 +136,13 @@ function sunLight(){
     matrix.led.set({r:0,g:0,b:0,w:0});
   }
 
-  //Insert sunRise function to take the calculations made here and translate into motor control
-
-
+    //trying to change the interval to give the serial port time to breathe
+    // setInterval(sunRise, 500);
+    sunRise();
 
 }
 
-function sunRise(xPos, yPos, zPos){
+function sunRise(){
   //Get sun position value
   //Transform sun position value into rotations from the arduino
   //Check to see if the connection is up
@@ -100,35 +152,65 @@ function sunRise(xPos, yPos, zPos){
   //Send protocol: <x,y,z>
   //Receive protocol: strings? Char?
 
-  // Read the port data
-  sol.on("open", () => {
-    console.log('serial port open');
-  });
-  parser.on('data', data =>{
-    switch(data) {
-      case "calibrating":
-        console.log("Sol is calibrating");
-        break;
-      case "ready":
-        console.log("Sol is ready");
-        console.log("Sending: " + '<' + xPos + ',' + yPos + ',' + zPos + '>');
-        sol.write('<' + xPos + ',' + yPos + ',' + zPos + '>');
-        break;
-      case "reached destination":
-        console.log("Sol reached destination –– sending new data");
+  //Map the altitude of the sun to motor coordinates
+  var sunPos = interpolate.interpolateNumber(0,maxHeight);
+  var tempMotorPos = Math.round(sunPos(sunFraction));
 
-        break;
-      case "same data":
-        console.log("Sol received the same data");
+  var solStatusSplit = solStatus.split(",");
 
+  switch(solStatusSplit[0]) {
+    case "Ready":
+      sol.write('c\n\r');
+    break;
+    case "Calibrating-Min":
+      console.log("Sol is calibrating");
+      break;
+    case "Calibrated":
+      console.log("Sol is ready");
+        //Check to see if the sun is above the horizon
+
+        if (prevMotorPos != tempMotorPos){
+          console.log("Maximum height: " + maxHeight);
+          console.log("Prev motor pose: " + prevMotorPos);
+          console.log("Temp motor pose: " + tempMotorPos);
+          motorPos = tempMotorPos;
+          console.log("Motor pose: " + motorPos);
+          sol.write('<' + motorPos + ',' + 0 + ',' + 0 + '>');
+          prevMotorPos = motorPos;
+        }
+        // console.log(motorPos);
+      break;
+    case "Arrived":
+
+    // console.log(solStatus);
+    if (sunAlt > 0){
+      //Check to see if the sun is above the horizon
+
+      if (prevMotorPos != tempMotorPos){
+        console.log("Maximum height: " + maxHeight);
+        console.log("Prev motor pose: " + prevMotorPos);
+        console.log("Temp motor pose: " + tempMotorPos);
+        console.log("Motor pose: " + motorPos);
+        motorPos = tempMotorPos;
+        sol.write('<' + motorPos + ',' + 0 + ',' + 0 + '>');
+        prevMotorPos = motorPos;
+      }
+      // console.log(motorPos);
     }
+      break;
+    case "same data":
+      console.log("Sol received the same data");
+      break;
+    default:
+      console.log(solStatus);
+  }
 
+  // sol.flush();
+    // var sunPos = interpolate.interpolateNumber(0,10000);
+    // motorPos = Math.round(sunPos(sunFraction));
+    // console.log(motorPos);
 
-    console.log('got word from arduino:', data);
-  });
-
-
-
+    // sunRise(motorPos, 0, 0);
 
 }
 
@@ -203,4 +285,21 @@ function getKey(keyName){
     var keys = JSON.parse(contents);
     return keys[keyName];
 // });
+}
+// ------ Check to see if the location file is empty ------ //
+function locationFileCheck (){
+  fs.readFile('currentLocation.json', 'utf8', function(err, contents){
+    if (err){
+      console.log("Something went wrong reading the location file.");
+      return err;
+    }
+    var emptyFile = !Object.keys(contents).length;
+    console.log("Is the file empty?" + " " + emptyFile);
+    if (emptyFile){
+      askForLocation();
+    }
+    else {
+    getLocationInfo();
+    }
+  });
 }
